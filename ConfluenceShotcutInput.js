3e116b44fb41456769a4552a13fb44486a7b1542
杰克有茶 <chenwan@xmfunny.com>
2022-03-31 09:39:38 +0800

    <feature> 现在支持评论、再编辑时的快捷键 <refactor> 简化代码逻辑

--- ConfluenceShotcutInput.js
+++ ConfluenceShotcutInput.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ConfluenceIndentAndOutdent
 // @namespace    http://tampermonkey.net/
-// @version      1.1.0
+// @version      1.2.0
 // @description  try to take over the world!
 // @author       杰克有茶
 // @match        docs.sofunny.io/*
@@ -13,130 +13,65 @@ const SHOTCUT_INDENT_CTRL_AND_KEY = "[";
 const SHOTCUT_OUTDENT_CTRL_AND_KEY = "]";
 
 const TIME_GAP = 300;
-let ifr;
-let ifrDocu;
+let iframe;
+let iframeDocu;
+let toolbar;
 let indentBtn;
 let outdentBtn;
-let saveBtn;
-let cancelBtn;
 
-let findMaintainTime;
-let isFinding = false;
+let hasBind = false;
 
-let isIndDown = false;
-let isOudDown = false;
+function MainTick() {
 
-function ResetState() {
-    ifr = undefined;
-    ifrDocu = undefined;
-    indentBtn = undefined;
-    outdentBtn = undefined;
-}
-
-function TickFindIFrame() {
     setInterval(() => {
-        if (ifr) {
-            return;
-        }
-        ifr = document.getElementById("wysiwygTextarea_ifr");
-        FindElement();
-    }, TIME_GAP);
-}
 
-function FindElement() {
-
-    if (ifr && ifrDocu && indentBtn && outdentBtn) {
-        return;
-    }
-
-    if (isFinding) {
-        return;
-    }
-
-    isFinding = true;
-
-    findMaintainTime = 5 * 10000;
-
-    let findInterval = setInterval(() => {
-
-        if (findMaintainTime <= 0) {
-            clearInterval(findInterval);
-            console.warn("检索失败, 不再检索");
-            isFinding = false;
-            return;
+        // ==== FIND iframe document ====
+        let m_iframe = document.getElementById("wysiwygTextarea_ifr");
+        if (m_iframe && m_iframe != iframe) {
+            iframe = m_iframe;
+            let _docu = m_iframe.contentWindow.document;
+            if (_docu && _docu != iframeDocu) {
+                iframeDocu = _docu;
+                hasBind = false;
+            }
         }
 
-        findMaintainTime -= TIME_GAP;
+        // ==== FIND toolbar ====
+        let m_tb = document.getElementById("toolbar");
+        if (m_tb && m_tb != toolbar) {
+            indentBtn = document.getElementById("rte-button-indent");
+            outdentBtn = document.getElementById("rte-button-outdent");
+            if (!indentBtn || !outdentBtn) {
+                return;
+            }
+            toolbar = m_tb;
+            hasBind = false;
+        }
 
-        if (!ifr) {
+        // ==== BIND keyup event Once ====
+        if (hasBind) {
             return;
         }
 
-        ifrDocu = ifr.contentWindow.document
-
-        indentBtn = document.getElementById("rte-button-indent");
-        outdentBtn = document.getElementById("rte-button-outdent");
-
-        if (ifr && ifrDocu && indentBtn && outdentBtn) {
-            BindShotcutKey();
-            TriggerCachedInput();
-            clearInterval(findInterval);
-            console.log("检索完成: 找到ind/oud");
-            isFinding = false;
+        let ptrDocu = iframeDocu ? iframeDocu : document;
+        ptrDocu.onkeyup = (e) => {
+            if (e.ctrlKey && e.key == SHOTCUT_INDENT_CTRL_AND_KEY) {
+                outdentBtn.click();
+            }
+            if (e.ctrlKey && e.key == SHOTCUT_OUTDENT_CTRL_AND_KEY) {
+                indentBtn.click();
+            }
         }
+
+        hasBind = true;
 
     }, TIME_GAP);
 }
 
-function BindShotcutKey() {
-    ifrDocu.onkeyup = function (e) {
-        if (e.ctrlKey && e.key == SHOTCUT_INDENT_CTRL_AND_KEY) {
-            outdentBtn.click();
-        }
-        if (e.ctrlKey && e.key == SHOTCUT_OUTDENT_CTRL_AND_KEY) {
-            indentBtn.click();
-        }
-    }
-    saveBtn = document.getElementById("rte-button-publish");
-    saveBtn.addEventListener("click", (e) => {
-        ResetState();
-    });
-
-    cancelBtn = document.getElementById("rte-button-cancel");
-    cancelBtn.addEventListener("click", (e) => {
-        ResetState();
-    });
-}
-
-function CacheInput(indDown) {
-    if (indDown) {
-        isIndDown = true;
-        setTimeout(() => {
-            isIndDown = false;
-        }, 300);
-    } else {
-        isOudDown = true;
-        setTimeout(() => {
-            isOudDown = false;
-        }, 300);
-    }
-}
-
-function TriggerCachedInput() {
-    if (isIndDown) {
-        indentBtn.click();
-    } else if (isOudDown) {
-        outdentBtn.click();
-    }
-}
-
-(function () {
+(function main() {
 
     'use strict';
 
-    TickFindIFrame();
-
-    FindElement();
-    BindRestart();
+    MainTick();
 
 })();
